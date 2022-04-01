c86769ff45b4f8b05e4bc2cbe3a07a2041e41920
marialovgren <marialovgren86@hotmail.com>
2022-04-01 09:06:06 +0200

    created simple todo

---
+++ app.js
@@ -0,0 +1,35 @@
+/**
+ * Vue.js Basics
+ *
+ */
+
+// Create Vue app
+const app = Vue.createApp({
+    data() {
+        return {
+            newTodoTitle: "",
+
+            todos: [
+                { title: "", completed: false },
+            ],
+        }
+    },
+    methods: {
+        createNewTodo() {
+            this.todos.push({ 
+                title: this.newTodoTitle, 
+                completed: false 
+            })
+        },
+
+        toggleTodo(todo) { 
+            todo.completed = !todo.completed 
+        },
+    },
+    computed: {
+
+    }
+})
+
+// Mount Vue app on the element with id `app` in the DOM
+app.mount('#app')
